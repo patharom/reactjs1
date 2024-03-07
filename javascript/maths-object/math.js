331dd88577b89e9ff9f3b90d8eec23211d5e396b
patharom <139317406+patharom@users.noreply.github.com>
2024-03-07 14:35:49 +0530

    Add files via upload

---
+++ javascript/maths-object/math.js
@@ -0,0 +1,141 @@
+//Math.E//
+
+// let math1 = Math.E
+// console.log(math1);
+
+//Math.PI
+
+// let math2 = Math.PI
+// console.log(math2);
+
+//math.round():-
+
+//  let math3 = Math.round(10.4)
+//  console.log(math3);
+
+//  let math4 = Math.round(10.5)
+//  console.log(math4);
+
+//  let math5 = Math.round(10)
+//  console.log(math5);
+
+
+//math.ceil():-
+
+// let math6 = Math.ceil(10.1)
+// console.log(math6);
+
+// let math7 = Math.ceil(10)
+// console.log(math7);
+
+//math.floor():-
+
+// let math8 = Math.floor(-10.9)
+// console.log(math8);
+
+// let math9 = Math.floor(-11.4)
+// console.log(math9);
+
+
+//Math.trunc():-
+
+// let math10 = Math.trunc(-1222.99)
+// console.log(math10);
+
+// let math11 = Math.trunc(1222.99)
+// console.log(math11);
+
+//Math.sign():-
+
+// let math12 = Math.sign(129059)
+// console.log(math12);
+
+// let math13 = Math.sign(354566)
+// console.log(math13);
+
+//Math.pow():-
+
+// let math14 = Math.pow(4 , 4)
+// console.log(math14);
+
+// let math15 = Math.pow(5 , 5)
+// console.log(math15);
+
+//Math.sqrt():-
+
+// let math16 = Math.sqrt(1024)
+// console.log(math16);
+
+// let math17 = Math.sqrt(1600)
+// console.log(math17);
+
+// let math18 = Math.sqrt(2500)
+// console.log(math18);
+
+//Math.abs():-
+
+// let math19  = Math.abs(20);
+// console.log(math19);
+
+// let math20 = Math.abs(30);
+// console.log(math20);
+
+// let math21 = Math.abs(40);
+// console.log(math21);
+
+
+//Math.min() and Math.max():-
+
+// let math22  = Math.min(101 , -20 , 60  , 87  , 50);
+// let math23  = Math.max(101 , 20 , 60  , 87  , 50);
+// console.log(math22);
+// console.log(math23);
+
+// let math24 = Math.min(10 , 20 , 30 , 40 , 50);
+// let math25 = Math.max(10 , 20 , 30 , 40 , 50);
+// console.log(math24);
+// console.log(math25);
+
+
+//Math.random():-
+
+// let math26  = Math.random()
+// console.log(math26);
+
+// let math27  = Math.random()
+// console.log(math27);
+
+//Math.sin():-
+
+// let math28 = Math.sin(30*3.14/180)	
+// console.log(math28);
+
+// let math29 = Math.sin(40*3.14/200)
+// console.log(math29);
+
+// let math30 = Math.sin(50*3.14/200)
+// console.log(math30);
+
+
+//Math.cos():-
+
+// let math31 = Math.cos(60*3.14/180);
+// console.log(math31);
+
+// let math32 = Math.cos(80*3.14/200);
+// console.log(math32);
+
+// let math33 = Math.cos(90*3.14/200);
+// console.log(math33);
+
+
+//Math.log():-
+
+// let math34 = Math.log2(15.27)
+// console.log(math34);
+
+// let math35 = Math.log2(20.25)
+// console.log(math35);
+
+// let math36 = Math.log2(25.15)
+// console.log(math36);
