1cdb64ee1cb3deb4c01fdaa765fb7726855d0edf
patharom <139317406+patharom@users.noreply.github.com>
2024-03-11 11:27:26 +0530

    Add files via upload

---
+++ javascript/dom/domevent/domEvent.js
@@ -0,0 +1,57 @@
+// /*  JavaScript HTML DOM Events */
+
+// function message(){
+//     alert("Yes, I Am Heading Tag")
+// }
+
+// function dateshow(){
+//     document.getElementById("date").innerHTML = Date();
+// }
+
+// function uppercase(){
+//     const change = document.getElementById("emails");
+//     change.value = change.value.touppercase();
+// }
+// function red(one){
+//     one.style.color = "red";
+// }
+// {
+//     function Down(obj){
+//         obj.style.backgroundcolor = "#1ec5e5";
+//         obj.innerHTML = "release me";
+//     }
+
+//     function up(obj) {
+//         obj.style.backgroundcolor="#D94A38";
+//         obj.innerHTML="Thank You";
+//     }
+// }
+
+
+// /* JavaScript addEventListener() */
+
+// {
+//     const x = document.getElementById("clickIt");
+//     const y = document.getElementById("hoverpara");
+
+//     x.addEventListener("click", Respondclick);
+//     y.addEventListener("mouseover", RespondMouseover);
+//     y.addEventListener("mouseout", RespondMouseOut);
+
+//     function RespondMouseover() {
+//         document.getElementById("effect").innerHTML +=
+//         "MouseOver Event" + "<br>";
+//     }
+
+//     function Respondmouseout() {
+//         document.getElementById("effect").innerHTML +=
+//         "MouseOut Event" + "<br>";
+//     }
+
+//     function RespondClick() {
+//         document.getElementById("effect").innerHTML +=
+//                   "Click Event" + "<br>";
+
+// }
+// }
+
