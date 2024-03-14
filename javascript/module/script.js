8adcad4179986c3ee2028867dcb387a925413a27
patharom <139317406+patharom@users.noreply.github.com>
2024-03-14 13:45:13 +0530

    Add files via upload

---
+++ javascript/module/script.js
@@ -0,0 +1,61 @@
+/* ES6 AKA Ecmascript 2015 */
+// import {login} from './lib.js'
+
+// login();
+
+/* let const */
+
+// var productName = 'Laptop';
+// //code polute
+
+// var age = 18;
+
+// if(age>=18){
+//     var driving = true;
+// }
+// console.log(driving);
+
+/* Function scope */
+
+// function register() {
+//     var username = 'rakesh';
+//     var password = 'secret';
+// }
+// console.log(username);
+
+
+/* var / let / const variables */
+
+// var product = 'laptop';
+// var product = 'mobile';
+
+// console.log(product);
+
+//  const product = 'laptop';
+// product = 'mobile';
+// console.log(product);
+
+
+/* Hoisting */
+
+//  console.log(age);
+// let age = 20;
+
+// let name;
+// name = 20;
+// console.log(name);
+
+/* window.product */
+
+// var box = "clothes"
+// let boxes = "toys"
+
+
+/* change const value using object */
+
+// const age = {
+//     years: 19,
+// };
+// age.years = 20;
+
+// console.log(age);
