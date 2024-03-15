6e259f75b2579ee8117958373bcfdb0e0daf9599
patharom <139317406+patharom@users.noreply.github.com>
2024-03-15 16:06:06 +0530

    Add files via upload

---
+++ javascript/array-method/Array/Array.js
@@ -0,0 +1,123 @@
+/* Array.every */
+/* Array.some */
+/* Array.entries */
+/* Array.reduse */
+/* for each */
+/* for in */
+/* for of */
+/* Array.keys */
+/* Html dom events */
+
+
+// const value1 = (currentValue) => currentValue > 18;
+
+// const Numbers1 = [45 , 89 , 20 , 18 , 25]
+
+// console.log(Numbers1.every(value1));
+
+
+// const value2 = (currentValue) => currentValue > 18;
+
+// const Numbers2 = [45 , 14 , 18 , 15 , 5]
+
+// console.log(Numbers2.some(value2));
+
+
+// const NewArray = ["javascript" , "SQL" , "Kite" , "JQuery"]
+
+// const values = NewArray.entries()
+
+// console.log(values.next().value);
+// console.log(values.next().value);
+// console.log(values.next().value);
+// console.log(values.next().value);
+// console.log(values.next().value);
+
+// const Num = [1 , 2 , 3 , 4]
+
+// const init = 0;
+
+// const vals = Num.reduce((val1 , val2) => val1 * val2);
+
+// console.log(vals);
+
+
+// // with initi value
+
+//const val = Num.reduce((val1 , val2) => val1 * val2 , init);
+
+//console.log(val);
+
+
+// const NewArry = ["india" ,  , "Austrlia" , "canada" , "brazil"];
+
+// const output  = NewArry.keys()
+
+// //const output  = NewArry
+
+// for(const keys of output){
+//   console.log(keys);
+// }
+
+// console.log(NewArry);
+
+// //const NewArry = ["india" ,  , "Austrlia" , "canada" , "brazil"];
+
+// const NewArray1 = [45 ,2 , 2 , 10]
+
+// NewArry.forEach(item => console.log(item + item));
+
+// NewArray1.forEach(item => console.log(item * item));
+
+/* For in loop */
+
+// const object = {
+//     FirstName : "Koladiya",
+//     LastName : "Rajan",
+//     Age : 25,
+// }
+
+// const NewArry = ["india" ,  , "Austrlia" , "canada" , "brazil"];
+
+// for(const values in NewArry){
+//   console.log(values);
+// }
+
+
+// const Str = "Good Morning , how are You! you are lazy person"
+
+// const Str1 = /[a-z]/g;
+
+// const Match = Str.match(Str1)
+
+// console.log(Match);
+
+
+/* Spred Oper */
+
+// const NewArry = ["india" ,  , "Austrlia" , "canada" , "brazil"];
+
+// const Arry = [...NewArry ,  "Norvay"]
+
+// console.log(NewArry);
+
+// console.log(Arry);
+
+
+// const regexp = /[a-z]/g;
+// const str = 'test1test2';
+
+// const array = [...str.matchAll(regexp)];
+
+// console.log(array);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[1]);
+// console.log(array[1]);
+// console.log(array[1]);
+// console.log(array[1]);
+
