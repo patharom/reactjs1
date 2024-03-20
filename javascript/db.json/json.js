793af2ce648fddf0a27e0bc3f0d64224022d6ad7
patharom <139317406+patharom@users.noreply.github.com>
2024-03-20 11:23:51 +0530

    Add files via upload

---
+++ javascript/db.json/json.js
@@ -0,0 +1,80 @@
+/* JavaScript JSON data */
+
+/* Mutable Data */
+
+
+// let Data = {name:"Jhon" , age : 50 , hobby : "Cricket"};
+
+// console.log(Data);
+
+// let Jsondata = {"name":"John" , "age":50 , "hobby" : "cricket"};
+
+// console.log(Jsondata);
+
+
+// let user = [
+//     {
+//         "name" : "john"
+//     },
+//     {
+//         "age" : 45
+//     },
+//     {
+//         "Hobby" :{
+//             "hobby1": "baseball",
+//             "hobby2": "superball",
+//             "hobby3": "cricket",
+//             "hobby4": "travelling"
+//         }
+//     },
+//     {
+//         "topic" : ["javascript" , "SQL" , "reactjs" , "html" , "css"]
+//     }
+// ]
+
+// console.log(User);
+// console.log(User[0].Name);
+// console.log(User[1].Age);
+// console.log(User[2].Hobby);
+// console.log(User[2].Hobby.hobby1);
+// console.log(User[2].Hobby.hobby2);
+// console.log(User[2].Hobby.hobby3);
+// console.log(User[2].Hobby.hobby4);
+// console.log(User[3].Topic);
+// console.log(User[3].Topic[0]);
+// console.log(User[3].Topic[1]);
+// console.log(User[3].Topic[2]);
+// console.log(User[3].Topic[3]);
+
+
+
+/* JSON.Parse Method */
+
+// let object =[
+//     {"name":"jhon" , "age" : 50 , "hobby" : "cricket"}
+// ];
+
+// let jsonobject = JSON.stringfy(object);
+
+// console.log(jsonobject);
+
+// console.log(typeof(jsonobject));
+
+
+// let jsonobject ='{"name":"jhon" , "age": 50, "hobby" : "cricket"}';
+
+// console.log(Object);
+
+// let object = JSON.parse(JsonObject);
+
+// console.log(Object);
+
+// console.log(typeof(JsonObject));
+// console.log(typeof(Object));
+
+// fetch('https://dummyjson.com/products')
+// .then(response => response.json())
+// .then(json => console.log(json));
+
+
+
